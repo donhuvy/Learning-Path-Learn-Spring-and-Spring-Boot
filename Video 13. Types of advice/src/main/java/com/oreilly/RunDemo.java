a985a425a86de87870b76be47828838d4e1defec
Do Nhu Vy <donhuvy@hotmail.com>
2016-10-22 23:24:35 +0700

    Types of advice

---
+++ Video 13. Types of advice/src/main/java/com/oreilly/RunDemo.java
@@ -0,0 +1,29 @@
+package com.oreilly;
+
+import com.oreilly.entities.Game;
+import com.oreilly.entities.Team;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class RunDemo {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Game game = context.getBean("game", Game.class);
+
+        Team royals = context.getBean("royals", Team.class);
+        Team redSox = context.getBean("redSox", Team.class);
+        Team cubs = context.getBean("cubs", Team.class);
+
+        game.setHomeTeam(royals);
+        game.setAwayTeam(cubs);
+        System.out.println(game.playGame());
+
+        game.setHomeTeam(cubs);
+        game.setAwayTeam(redSox);
+        System.out.println(game.playGame());
+
+        context.close();
+    }
+
+}
---
+++ Video 13. Types of advice/src/main/java/com/oreilly/entities/BaseballGame.java
@@ -0,0 +1,69 @@
+package com.oreilly.entities;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.sql.DataSource;
+
+public class BaseballGame implements Game {
+
+    private Team homeTeam;
+    private Team awayTeam;
+    private DataSource dataSource;
+
+    public BaseballGame() {
+
+    }
+
+    public BaseballGame(Team homeTeam, Team awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+    }
+
+    @PostConstruct
+    public void startGame(){
+        System.out.println("Playing National Athem");
+    }
+
+    @PreDestroy
+    public void endGame(){
+        System.out.println("Sending highlight to MLB");
+    }
+
+    @Override
+    public void setHomeTeam(Team homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    @Override
+    public Team getHomeTeam() {
+        return homeTeam;
+    }
+
+    @Override
+    public void setAwayTeam(Team awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    @Override
+    public Team getAwayTeam() {
+        return awayTeam;
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public String playGame() {
+        return Math.random() < 0.5 ? getHomeTeam().getName() : getAwayTeam().getName();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Game between %s at %s", awayTeam.getName(), homeTeam.getName());
+    }
+}
